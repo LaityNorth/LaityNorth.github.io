d9dfe9fae01b571ab8e0b8a322102f297e3bf249
luoxiangkaids <luoxiangkaids@163.com>
2024-07-01 00:32:27 +0800

    vitepress侧边栏路由优化

--- .vitepress/sidebar/distributedlock.js
+++ .vitepress/sidebar/distributedlock.js
@@ -1,13 +1,14 @@
 // 分布式锁相关的侧边栏
+const basePath = "/分布式锁/"
 export default [
     {
         text: '分布式锁',
         // collapsed: false,
         items: [
-            { text: '数据库锁案例', link: '/分布式锁/基于数据库的乐观锁和悲观锁' },
-            { text: 'Redis分布式锁案例', link: '/分布式锁/基于Redis的方式分布式锁' },
-            { text: 'Readisson分布式锁案例', link: '/分布式锁/基于Redisson的方式实现分布式锁' },
-            { text: 'Zookeeper分布式锁案例', link: '/分布式锁/基于Zookeeper的方式实现分布式锁' },
+            { text: '数据库锁案例', link: `${basePath}基于数据库的乐观锁和悲观锁`},
+            { text: 'Redis分布式锁案例', link: `${basePath}基于Redis的方式分布式锁` },
+            { text: 'Readisson分布式锁案例', link: `${basePath}基于Redisson的方式实现分布式锁`},
+            { text: 'Zookeeper分布式锁案例', link: `${basePath}基于Zookeeper的方式实现分布式锁`},
 
         ]
     }
--- .vitepress/sidebar/environment.js
+++ .vitepress/sidebar/environment.js
@@ -1,28 +1,29 @@
 // 环境安装
+const basePath = "/环境搭建/"
 export default [
     {
         text: 'Linux环境搭建',
         // collapsed: false,
         items: [
-            { text: '安装JDK', link: '/环境搭建/Linux上安装Jdk1.8' },
-            { text: '安装Redis', link: '/环境搭建/Linux上安装Redis' },
-            { text: '安装MySQL', link: '/环境搭建/Linux上安装MYSQL' },
-            { text: '安装MongoDB', link: '/环境搭建/Linux安装MongoDB' },
-            { text: '安装Nginx', link: '/环境搭建/Linux上安装Nginx' },
-            { text: '安装RabbitMQ', link: '/环境搭建/Linux上安装RabbitMQ' },
-            { text: '安装Nacos', link: '/环境搭建/Linux上部署Nacos集群' },
-            { text: '安装Jenkins', link: '/环境搭建/Linux系统安装Jenkins' },
-            { text: '安装Docker', link: '/环境搭建/Linux系统安装Docker' },
-            { text: '安装Maven', link: '/环境搭建/Linux安装Maven' },
+            { text: '安装JDK', link: `${basePath}Linux上安装Jdk1.8` },
+            { text: '安装Redis', link: `${basePath}Linux上安装Redis` },
+            { text: '安装MySQL', link: `${basePath}Linux上安装MYSQL` },
+            { text: '安装MongoDB', link: `${basePath}Linux安装MongoDB`},
+            { text: '安装Nginx', link: `${basePath}Linux上安装Nginx`},
+            { text: '安装RabbitMQ', link: `${basePath}Linux上安装RabbitMQ`},
+            { text: '安装Nacos', link: `${basePath}Linux上部署Nacos集群`},
+            { text: '安装Jenkins', link: `${basePath}Linux系统安装Jenkins`},
+            { text: '安装Docker', link: `${basePath}Linux系统安装Docker`},
+            { text: '安装Maven', link: `${basePath}Linux安装Maven`},
 
         ]
     },
     {
         text: 'Windos环境搭建',
         items: [
-            { text: '安装Nacose', link: '/环境搭建/Windows系统Nacos安装' },
-            { text: '安装MongoDB', link: '/环境搭建/Windows安装MongoDB' },
-            { text: '解压安装MySQL', link: '/环境搭建/Windows解压安装MYSQL5.7' },
+            { text: '安装Nacose', link: `${basePath}Windows系统Nacos安装`},
+            { text: '安装MongoDB', link: `${basePath}Windows安装MongoDB`},
+            { text: '解压安装MySQL', link: `${basePath}Windows解压安装MYSQL5.7`},
         ]
     }
 ]
